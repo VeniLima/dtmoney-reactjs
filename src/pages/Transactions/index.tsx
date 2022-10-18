1d49ae233ba4967e2d9db0049139bce78e34c80e
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-10-18 09:54:24 -0300

    Transactions table

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,47 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$12.440.12</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>18/10/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$10.448.12</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>18/10/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$2.440.12</PriceHighlight>{" "}
+              </td>
+              <td>Venda</td>
+              <td>18/10/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
