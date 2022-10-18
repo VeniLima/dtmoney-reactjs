3a36e6de2d922ea394e0771cedb91adcd6be332e
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-10-18 12:01:10 -0300

    modal new transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
-import logoImg from '../../assets/logo.svg'
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
 
-export function Header(){
-    return(
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={logoImg} alt=''/>
-                
-                <NewTransactionButton>Nova transação</NewTransactionButton>
-            </HeaderContent>
-        </HeaderContainer>
-      
-    )
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
