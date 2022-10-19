8bcb952e52f48f63450c40b2ee4b98397e2522b1
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-10-19 12:07:23 -0300

    radio button style at modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,13 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, Overlay } from "./styles";
+
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -8,15 +16,29 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
 
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
         <form action="">
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionType>
+            <TransactionTypeButton value="income" variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton value="outcome" variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">Cadastrar</button>
         </form>
-
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   );
